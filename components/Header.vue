d42a483abe8335d0ce961a1eaa16903d53319785
maliksvd <malik.krechiche@gmail.com>
2022-03-18 01:23:38 -0400

    fix: compress img and fix performance lighthouse error

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,12 @@
 <template>
   <section>
     <div class="">
-      <nuxt-img src="header.png" class="pointer-events-none w-full h-96" alt="" />
+      <nuxt-img src="header.jpg" class="pointer-events-none w-full h-96" alt="" />
       <NuxtLink to="/">
         <nuxt-img
-          src="avatar.png"
+          src="avatar.jpg"
+          width="128"
+          height="128"
           class="-mt-24 pointer-events-none md:-mt-44 w-32 h-32 rounded-full mx-auto"
           alt="Avatar of Malik"
         />
