f7661eacef2f69f02030ca98f2fed314dce66ee6
maliksvd <malik.krechiche@gmail.com>
2022-03-18 01:06:44 -0400

    fix: path error

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <section>
     <div class="">
-      <nuxt-img src="header2.png" class="w-full h-96" alt="" />
+      <nuxt-img src="header.png" class="pointer-events-auto w-full h-96" alt="" />
       <NuxtLink to="/">
         <nuxt-img
           src="avatar.png"
-          class="-mt-24 md:-mt-44 w-32 h-32 rounded-full mx-auto"
+          class="-mt-24 pointer-events-auto md:-mt-44 w-32 h-32 rounded-full mx-auto"
           alt="Avatar of Malik"
         />
       </NuxtLink>
